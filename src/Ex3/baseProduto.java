cc7cbbdbd4c4761b8fa5bab1067496fdfd844120
aluno <Aluno@DEV007168.educacional.up>
2022-08-29 21:10:23 -0300

    init2

--- src/Ex3/baseProduto.java
+++ src/Ex3/baseProduto.java
@@ -3,13 +3,15 @@ package Ex3;
 public class baseProduto implements iProduto {
 
 	public String Nome;
+	public double Peso;
+	public double Volume;
 	public double Preco;
 	public double txFrete;
 	
 	
 	@Override
 	public void calculaFrete() {
-		this.Preco += this.Preco * (this.txFrete);
+		this.Preco += (this.Peso/1000) * 0.8  + (this.Volume/1000000) + (this.Preco * (this.txFrete));
 	}
 
 	
